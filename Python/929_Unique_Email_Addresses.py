4d48e3ecee7c22401599a6c51deefa03bb4e465a
Imed Adel <geekemad@gmail.com>
2019-01-15 14:13:51 +0100

    Update 929_Unique_Email_Addresses.py

--- Python/929_Unique_Email_Addresses.py
+++ Python/929_Unique_Email_Addresses.py
@@ -4,14 +4,10 @@ class Solution:
         :type emails: List[str]
         :rtype: int
         """
-        cleanEmails = set()
-        
+        ans = set()
         for email in emails:
-            local, domain = email.split("@")
-            
-            if "+" in local:
-                local = local[:local.index("+")]
-            local = local.replace(".","")
-            cleanEmails.add(local + "@" + domain)
-        
-        return len(cleanEmails)
+            local,domain = email.split('@')
+            if '+' in local: local = local[:local.index('+')]
+            local = local.replace('.', '')
+            ans.add(local+'@'+domain)
+        return len(ans)
