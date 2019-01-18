02f6ea4f7bf9392b021571e7c89e9c29bbc85c6d
Imed Adel <geekemad@gmail.com>
2019-01-18 14:59:09 +0100

    Create 559_Maximum_Depth_of_N-ary_Tree.py

---
+++ Python/559_Maximum_Depth_of_N-ary_Tree.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        stack = []
+        if root is not None:
+            stack.append((1, root))
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                for c in root.children:
+                    stack.append((current_depth + 1, c))
+        return depth
