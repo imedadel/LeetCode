7a6897105bd65bec91abde739a8a019e0301479e
Imed Adel <geekemad@gmail.com>
2019-01-19 10:47:58 +0100

    Create 811_Subdomain_Visit_Count.py

---
+++ Python/811_Subdomain_Visit_Count.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subdomainVisits(self, cpdomains):
+        """
+        :type cpdomains: List[str]
+        :rtype: List[str]
+        """
+        ans = collections.Counter()
+        for cpdomain in cpdomains:
+            count, domain = cpdomain.split()
+            count = int(count)
+            frags = domain.split(".")
+            for i in range(len(frags)):
+                ans[".".join(frags[i:])] += count
+        # return ["{} {}".format(ct, dom) for dom, ct in ans.items()]
+        return [str(ct)+" "+dom for dom, ct in ans.items()]
